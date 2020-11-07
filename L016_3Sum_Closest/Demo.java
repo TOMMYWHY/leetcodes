fec756e0863ff9ea66b9984273eab1cd91d91737
tommy <tommywhy1989@gmail.com>
2020-11-07 09:00:36 +0800

    16

---
+++ L016_3Sum_Closest/Demo.java
@@ -0,0 +1,29 @@
+class Demo{
+    public static void main(String[] args) {
+        
+    }
+}
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int result = nums[0]+nums[1]+nums[2];
+        Arrays.sort(nums);
+        for(int i =0; i<nums.length-2; i++){
+            int start = i + 1;
+            int end = nums.length -1;
+            while (start<end){
+                int sum = nums[i]+nums[start]+nums[end];
+                if(sum > target){
+                    end--;
+                }else if(sum < target){
+                    start++;
+                }else{
+                    return target;//equal
+                }
+                if(Math.abs(target - sum) < Math.abs(target - result)){
+                    result = sum;
+                }
+            }
+        }
+        return result;
+    }
+}
